91714d2d65e205dabc3c2edf37ae3fbd1fb175ff
Jankulovska <jankulovskasara60@gmail.com>
2023-12-05 13:51:32 +0100

    Add first program

---
+++ src/zadacaaa32/Glavna.java
@@ -0,0 +1,14 @@
+package zadacaaa32;
+
+public class Glavna {
+	public static void main(String [] args) {
+		Student marko = new Student();
+		marko.setId(1);
+		marko.setIme("Marko");
+		marko.setGodina(18);
+		System.out.println("Reden broj" + marko.getId() + " e student" + marko.getIme()+ "star" + marko.getGodina() + "godina");
+				
+				
+	}
+
+}
---
+++ src/zadacaaa32/Student.java
@@ -0,0 +1,25 @@
+package zadacaaa32;
+
+public class Student {
+int id;
+String ime;
+int godina;
+public int getId() {
+	return id;
+	}
+public void setId(int id) {
+	this.id = id;
+}
+public String getIme() {
+	return ime;
+}
+public void setIme(String ime) {
+	this.ime = ime;
+	}
+public int getGodina() {
+	return godina;
+	}
+public void setGodina(int godina) {
+	this.godina=godina;
+}
+}
